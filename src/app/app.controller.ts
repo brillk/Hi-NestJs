5c61ca90817aa4ba3149ea28134c4361db4a3e38
KJH <stonehead334@gmail.com>
2022-05-30 16:36:27 +0900

    Testing movies

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller,Get } from '@nestjs/common';
 export class AppController {
     @Get()
     home() {
-        return "Welcome Home"
+        return 'Hello World!';
     }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,41 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ (GET)', () => { //url 요청을 테스팅하고 있다
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
   });
+  /* 보통 테스팅을 할땐 두개의 서버를 쓴다. 
+  하나는 테스팅, 둘은 실제로 쓰는 서버
+  그래서 DB에선 데이터를 생성, 삭제하는 일이 빈번한데 */
+
+
+  describe("/movies", () => {
+
+    it("GET", () => {
+      return request(app.getHttpServer())
+      .get('/movies')
+      .expect(200)
+      .expect([]);
+    })
+
+    it('POST', () =>{
+      return request(app.getHttpServer())
+      .post("/movies")
+      .send({
+        title: 'Test',
+        year: 2000,
+        genres: ['test'],
+      })
+      .expect(201);
+    })
+
+    it("DELETE", () => {
+      return request(app.getHttpServer())
+      .delete("/movies")
+      .expect(404);
+    })
+  })
 });
