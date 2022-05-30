c2238ab69d6851eeecc5e31a921b5eb6d5dceded
KJH <stonehead334@gmail.com>
2022-05-30 09:22:30 +0900

    Ready for service

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,8 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+/* NestJs의 구조와 아키텍처
+NestJs는 컨트롤러를 비즈니스 로직이랑 구분 짓고 싶어한다
+
+@Controller -> 이건 그냥 url를 가져오고, function을 리턴한다
+*/
