42930a9959061103b997f6649b1074f0e52169d7
KJH <stonehead334@gmail.com>
2022-05-30 14:31:31 +0900

    Testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,8 +16,36 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  // 테스트 해보기
-  // it('test1', () => {
-  //   expect(2+2).toEqual(5);
-  // })
+  
+  describe('getAll', () => {
+    it("should return an array", () => {
+      // MovieService를 불러와서 작성 가능
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    })
+  })
+  describe('getOne', () => {
+    it("should be return a movie", () => {
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000,
+    });
+    
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Movie 999 Not Found`)
+      }
+    })
+  
+  })
+
+
 });
