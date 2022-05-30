8f5e3028e3410c8dba0349a62be5c09c039e131e
KJH <stonehead334@gmail.com>
2022-05-30 10:55:09 +0900

    Movie Service Part-1

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,10 @@
+/*
+movie의 요소를 적는다
+
+*/
+export class Movie {
+    id: number;
+    title: string;
+    year: number;
+    genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,46 +1,47 @@
 import { Controller, Get, Param, Post, Delete, Patch, Put, Body, Query} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+
 
 @Controller('movies')
 export class MoviesController {
+
+    // 요청을 해서 services에 접근하자
+    constructor(private readonly moviesService: MoviesService) {}
+
     @Get()
-    getAll() {
-        return "This will return all movies"
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
-    @Get("search") // movies/search?year=2000
-    search(@Query('year') year: string) {
-        return `search movie ${year}` // :id url밑으로 쓰면 연결이 안됨
-    }
+    // @Get("search") // movies/search?year=2000
+    // search(@Query('year') year: string) {
+    //     return `search movie ${year}` // :id url밑으로 쓰면 연결이 안됨
+    // }
 
-    @Get("/:id")
-    getOne(@Param('id') MovieId: string) {// parameter를 요청
-        return `Movie : ${MovieId}`
+    @Get(":id")
+    getOne(@Param('id') MovieId: string):Movie {// parameter를 요청
+        return this.moviesService.getOne(MovieId);
     }
 
     @Post()
-    create() {
-        return "create movie"
+    create(@Body() movieData) {
+        return this.moviesService.create(movieData);
     }
 
-    @Delete("/:id") 
+    @Delete(":id") 
     delMovie(@Param('id') MovieId: string) {
-        return `Delete Movie: ${MovieId}`
+        return this.moviesService.deleteOne(MovieId);
     }
 
     @Patch("/:id") 
     updateMovie(@Param('id') MovieId: string, @Body() updateData) {
         return {
             updateMovie: MovieId,
-            ...updateData,
+            ...updateData, // ... -> object
         }
     }
-    
-    @Put("/:id") 
-    updateAllMovie(@Param('id') MovieId: string) {
-        return `Update All Movie : ${MovieId}`
-    }
-
-   
 }
 /*
 기억하자, 무언가가 필요하다면 내가 요청해야 한다
